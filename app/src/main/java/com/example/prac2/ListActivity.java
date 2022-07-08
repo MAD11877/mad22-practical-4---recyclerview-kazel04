33a2edadfcdcb8f8efc4a5dc8f081684562bba90
Kazel <s10222282@connect.np.edu.sg>
2022-07-08 17:49:17 +0800

    update for practical

--- app/src/main/java/com/example/prac2/ListActivity.java
+++ app/src/main/java/com/example/prac2/ListActivity.java
@@ -1,6 +1,7 @@
 package com.example.prac2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -12,164 +13,38 @@ import android.os.Handler;
 import android.widget.Button;
 import android.content.DialogInterface;
 import android.app.AlertDialog;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.Console;
-import java.util.Arrays;
-import java.util.Random;
 import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.jar.Attributes;
-
 
 public class ListActivity extends AppCompatActivity {
-
-    RecyclerView recyclerView;
-    RecyclerView.Adapter programAdapter;
-    RecyclerView.LayoutManager layoutManager;
-
     AlertDialog.Builder builder;
-    ArrayList<User> Userlist = new ArrayList<>();
-    public void CreateList (ArrayList<User> Userlist)
-    {
-        int nums = 20;
-        Random rand = new Random(); //instance of random class
-        int upperbound = 10000000;
-
-        // for each loop, create a new user and add to list
-        for (int i = 0; i < nums; i++)
-        {
-            //generate random values from 0-24
-            int int_random = rand.nextInt(upperbound);
-            String descrips = Integer.toString(int_random);
-
-            User usernew = new User("Name"+int_random,""+descrips,i,true );
-            Userlist.add(usernew);
-
-        }
-
-    }
-
-    //Creating name list
-    ArrayList<String> Namelist = new ArrayList<>();
-    public void CreateNameList (ArrayList<String> namelist, ArrayList<User> Userlist)
-    {
-        int nums = 20;
-
-        // for each loop, create a new user and add to list
-        for (int i = 0; i < nums; i++)
-        {
-
-            namelist.add(Userlist.get(i).name);
-        }
-
-    }
-
-    //Creating description list
-    ArrayList<String> Descriplist = new ArrayList<>();
-    public void CreateDescripList (ArrayList<String> descriplist, ArrayList<User> Userlist)
-    {
-        int nums = 20;
-
-        // for each loop, create a new user and add to list
-        for (int i = 0; i < nums; i++)
-        {
-
-            descriplist.add(Userlist.get(i).description);
-        }
-
-    }
-
-
-
-
-    public class BrandViewHolder extends RecyclerView.ViewHolder {
-        TextView txt;
-        public BrandViewHolder(View itemView) {
-            super(itemView);
-            txt = itemView.findViewById(android.R.id.text1);
-        }
-    }
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-        recyclerView = findViewById(R.id.rvProgram);
-        recyclerView.setHasFixedSize(true);
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-
-        //Actually creating the lists
-        CreateList(Userlist);
-        CreateNameList(Namelist,Userlist);
-        CreateDescripList(Descriplist,Userlist);
-
-        //converting to string array
-        String Names[] = Namelist.toArray(new String[Namelist.size()]);
-        String Descrips[] = Descriplist.toArray(new String[Descriplist.size()]);
-
-        int[] images= {R.drawable.ic_launcher_foreground,R.drawable.ic_launcher_foreground,R.drawable.ic_launcher_foreground};
-
-
-
-
-        //setting the lists here
-        programAdapter = new ProgramAdapter(this,Names,Descrips,images);
-        recyclerView.setAdapter(programAdapter);
-
-        RecyclerView rlayout = findViewById(R.id.rvProgram);
-
-        rlayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                builder.setMessage(R.string.dialog_message).setTitle(R.string.dialog_title);
-                //Setting message manually and performing action on button click
-                builder.setMessage("MADness")
-                        .setCancelable(false)
-                        .setPositiveButton("View", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                // Insert random integer generation + transfer to main activity here!
-                                int randomNum = ThreadLocalRandom.current().nextInt(0, 100000 + 1);
-                                String value= String.valueOf(randomNum);
-                                Intent i = new Intent(ListActivity.this, MainActivity.class);
-                                i.putExtra("key",value);
-                                startActivity(i); //code changes activity to mainactivity!
-
-                                Toast.makeText(getApplicationContext(), value,
-                                        Toast.LENGTH_SHORT).show();
-                            }
-                        })
-                        .setNegativeButton("Close", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                //  Action for 'NO' Button
-                                dialog.cancel();
-                                Toast.makeText(getApplicationContext(), "you choose no action for alertbox",
-                                        Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                //Creating dialog box
-                AlertDialog alert = builder.create();
-                //Setting the title manually
-                alert.setTitle("AlertDialogExample");
-                alert.show();
-
-
-            }
-        });
-
-
-
-
-
-
+        createList();
 
     }
+    public void createList(){
+        ArrayList<User> UserList = new ArrayList<>();
+        for(int i = 0; i < 20; i++){
+            User Usersample = new User("Name" + randomnum(), "Description " + randomnum(), i, (Math.random() < 0.5));
+            UserList.add(Usersample);
+        }
+        RecyclerView rv = findViewById(R.id.recyclerview);
+        Adapter adapter = new Adapter(UserList/*, this::onNoteClick*/);
+        LinearLayoutManager layout = new LinearLayoutManager(this);
+        rv.setLayoutManager(layout);
+        rv.setItemAnimator(new DefaultItemAnimator());
+        rv.setAdapter(adapter);
+    }
+    public Integer randomnum(){
+        int num = (int) Math.round(Math.random() * 1000000);
+        return num;
+    }
 }
+
+//worked on this practical with RayZin
--- app/src/main/java/com/example/prac2/MainActivity.java
+++ app/src/main/java/com/example/prac2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.prac2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,19 +16,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // textview changes (if some part of the layout is not there, must force refresh the layout!)
-        TextView tv1 = (TextView)findViewById(R.id.textView5);
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            String value = extras.getString("key");
-            tv1.setText(value);
-        }
-
         Button button = (Button) findViewById(R.id.button3);
         String buttonText = (String) button.getText();
         System.out.println(buttonText);
         User user1 = new User("Satsuma","template",1,true );
 
+        // textview changes (if some part of the layout is not there, must force refresh the layout!)
+        TextView tv1 = (TextView)findViewById(R.id.textView2);
+        Intent newIntent = getIntent();
+        String numbers = newIntent.getStringExtra("name");
+
+        Bundle extras = getIntent().getExtras();
+        tv1.setText(numbers);
         if(user1.followed = true){
             button.setText("Unfollow");
             button.setOnClickListener(new View.OnClickListener() {
@@ -49,6 +49,12 @@ public class MainActivity extends AppCompatActivity {
             });
         }
 
+    }
+
+    /** Called when the user taps the Message button (referred from https://developer.android.com/training/basics/firstapp/starting-activity#java)*/
+    public void messagetransit(View view) {
+        Intent intent = new Intent(this, MessageGroup.class);
+        startActivity(intent);
     }
 // abc
 
